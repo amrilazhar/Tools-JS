0f01ddc2b604c5b320a7144b1dcf488c577310e4
amril <amrilazhar@gmail.com>
2021-03-18 18:43:34 +0700

    -bug fix after recording, cannot record another video, after stop, generate dowload link and save video on stop sharing (not jus on stop recording)

--- ScreenRecorder/ScreenRecorder.js
+++ ScreenRecorder/ScreenRecorder.js
@@ -10,78 +10,105 @@ const stopElem = document.getElementById("stop");
 const startBtn = document.getElementById("startBtn");
 const stopBtn = document.getElementById("stopBtn");
 const downloadBtn = document.getElementById("downloadBtn");
-const dateNow = new Date();
 
+let tracks;
 //Recorder buat ngerekam
 let recorder;
-const chunks = [];
+let chunks = [];
 
 // Options for getDisplayMedia()
 var displayMediaOptions = {
   video: {
-    cursor: "always"
+    cursor: "always",
   },
-  audio: false
+  audio: false,
 };
 
 // Set event listeners for the start and stop buttons
-startElem.addEventListener("click", function(evt) {
-  startCapture();
-  startBtn.classList.add("hide");
-  stopBtn.classList.remove("hide");
-  downloadBtn.classList.add("hide");
-}, false);
+startElem.addEventListener(
+  "click",
+  function (evt) {
+    startCapture();
+    startBtn.classList.add("hide");
+    stopBtn.classList.remove("hide");
+    downloadBtn.classList.add("hide");
+  },
+  false
+);
 
-stopElem.addEventListener("click", function(evt) {
-  stopCapture();
-  startBtn.classList.remove("hide");
-  stopBtn.classList.add("hide");
-  downloadBtn.classList.remove("hide");
-}, false);
+stopElem.addEventListener(
+  "click",
+  function (evt) {
+    stopCapture();
+    startBtn.classList.remove("hide");
+    stopBtn.classList.add("hide");
+    downloadBtn.classList.remove("hide");
+  },
+  false
+);
 
-
-
-console.log = msg => logElem.innerHTML += `${msg}<br>`;
-console.error = msg => logElem.innerHTML += `<span class="error">${msg}</span><br>`;
-console.warn = msg => logElem.innerHTML += `<span class="warn">${msg}<span><br>`;
-console.info = msg => logElem.innerHTML += `<span class="info">${msg}</span><br>`;
-
+console.log = (msg) => (logElem.innerHTML += `${msg}<br>`);
+console.error = (msg) =>
+  (logElem.innerHTML += `<span class="error">${msg}</span><br>`);
+console.warn = (msg) =>
+  (logElem.innerHTML += `<span class="warn">${msg}<span><br>`);
+console.info = (msg) =>
+  (logElem.innerHTML += `<span class="info">${msg}</span><br>`);
 
 // Share Screen //
 async function startCapture() {
   logElem.innerHTML = "";
 
   try {
-    videoElem.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+    videoElem.srcObject = await navigator.mediaDevices.getDisplayMedia(
+      displayMediaOptions
+    );
     recorder = new MediaRecorder(videoElem.srcObject);
-    recorder.ondataavailable = e => chunks.push(e.data);
+    recorder.ondataavailable = (e) => chunks.push(e.data);
     recorder.start();
+
+    //initiate tracks
+    tracks = videoElem.srcObject.getTracks()
+    let vidTracks = videoElem.srcObject.getVideoTracks();
+    //add listener if someone click stopsharing
+    vidTracks.forEach((track) => {
+      track.onended = () => {
+        stopCapture();
+        startBtn.classList.remove("hide");
+        stopBtn.classList.add("hide");
+        downloadBtn.classList.remove("hide");
+      }
+    });
+
     dumpOptionsInfo();
-    console.log("<span class=\"info\"> Start Recording </span>");
-  } catch(err) {
+    console.log('<span class="info"> Start Recording </span>');
+  } catch (err) {
     console.error("Error: " + err);
   }
 }
 
 function stopCapture(evt) {
-  let tracks = videoElem.srcObject.getTracks();
   let fileName;
   recorder.stop();
   /// Convert to Object and save
-  recorder.onstop = e => {
-    const completeBlob = new Blob(chunks, { type: 'video/webm' });
+  recorder.onstop = (e) => {
+    const completeBlob = new Blob(chunks, { type: "video/webm" });
+    const dateNow = new Date();
+
     let DLink = document.getElementById("recorded");
     DLink.href = URL.createObjectURL(completeBlob);
     DLink.download = `scRecord-${dateNow.getDate()}-${dateNow.getMonth()}-${dateNow.getFullYear()}-${dateNow.getMilliseconds()}`;
     DLink.innerHTML = "Click here to download the video";
     fileName = `scRecord-${dateNow.getDate()}-${dateNow.getMonth()}-${dateNow.getFullYear()}-${dateNow.getMilliseconds()}`;
-  }
+    //empty the chunks to use again
+    chunks = [];
+  };
 
-  console.log("<span class=\"info\"> STOP Recording </span>");
+  console.log('<span class="info"> STOP Recording </span>');
 
-  tracks.forEach(track => track.stop());
+  tracks.forEach((track) => track.stop());
   videoElem.srcObject = null;
-  console.log("<span class=\"info\"> STOP Capturing </span>");
+  console.log('<span class="info"> STOP Capturing </span>');
 }
 
 function dumpOptionsInfo() {
